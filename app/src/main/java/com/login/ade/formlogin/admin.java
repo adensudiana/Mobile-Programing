1bf4b199503a69c58b7d0825ea11a4888a847b28
adensudiana <adensudiana@gmail.com>
2016-09-30 09:23:56 +0700

    login view 2

---
+++ app/src/main/java/com/login/ade/formlogin/admin.java
@@ -0,0 +1,17 @@
+package com.login.ade.formlogin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.*;
+
+public class admin extends Activity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin);
+
+    }
+}
+
